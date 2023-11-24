e01bb8de7a39a8a5ee1475b5019835ba628bbfe2
hcmgr <harryrackemann@gmail.com>
2023-11-24 14:56:59 +1000

    working message passing between cli and server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,9 @@
 mod message;
 use message::Message;
 
+use std::io::{self, Read, Write};
+use std::net::TcpStream;
+
 /**
 Represents the main wrapper for all protocol messages. 
 */
@@ -10,12 +13,23 @@ pub struct Packet {
     pub buffer: Vec<u8>,
 }
 
-fn main() {
+fn handle_client(ser_message: Vec<u8>, mut stream: TcpStream) -> io::Result<()> {
+    stream.write_all(&ser_message)?;
+    let mut buffer = [0u8; 1024];
+    let bytes_read = stream.read(&mut buffer)?;
+    let der_message = Message::deserialize(&buffer[..bytes_read])
+        .unwrap_or_else(|| {
+            println!("Failed to deserialize");
+            Message::empty()
+        });
+    println!("{:?}", der_message);
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
     let message = Message::new("hcmgr", "jess", "Morning darling");
-    let buffer: Vec<u8> = message.serialize();
-    let test_message = match Message::deserialize(&buffer) {
-        Some(tm) => tm,
-        None => panic!("invalid message")
-    };
-    println!("{:?}", test_message);
+    let ser_message: Vec<u8> = message.serialize();
+    let stream = TcpStream::connect("127.0.0.1:8080")?;
+    handle_client(ser_message, stream)?;
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,20 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::{self, Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
+    println!("client connected");
+    let mut buffer = [0u8; 1024];
+    let bytes_read = stream.read(&mut buffer)?;
+    println!("read {} bytes from client", bytes_read);
+    stream.write_all(&buffer[..bytes_read]).unwrap();
+    Ok(())
 }
